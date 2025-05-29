fddea9132f262b5043d0a6549116a46413a808aa
Subhash Pavan Chakravarthy Somarouthu <sspchakravarthy@gmail.com>
2025-05-28 22:16:24 -0400

    merge files .ipynb

--- merge.py
+++ merge.py
@@ -3,12 +3,13 @@ import nbformat
 from nbformat.v4 import new_notebook
 
 
-folder_path = 'Case_Study1/src/notebook'
+folder_path = 'Computer_Vision\case_study1\src'
 
 
 notebook_files = [f for f in os.listdir(folder_path) if f.endswith('.ipynb') and not f.startswith('.')]
 
-notebook_files.sort()
+notebook_files = sorted(notebook_files, key=lambda x: (x != 'Assignment1DataPreprocessing.ipynb', x))
+
 
 
 combined_nb = new_notebook()
@@ -27,7 +28,7 @@ for nb_file in notebook_files:
 combined_nb.cells = combined_cells
 
 
-output_path = os.path.join(folder_path, 'Combined_Notebook.ipynb')
+output_path = os.path.join(folder_path, 'AISC2008_CaseStudy1_combined.ipynb')
 with open(output_path, 'w', encoding='utf-8') as f:
     nbformat.write(combined_nb, f)
 
