4f9319e77f789dfa3311344cc8a7e5b807a494bf
anjusunnyc <anjusunnyv@gmail.com>
2025-07-30 23:21:37 -0400

    Updated config file with hyperparameter value

--- Deployment/gan_api/config.py
+++ Deployment/gan_api/config.py
@@ -7,6 +7,9 @@ class Config:
     MODEL_PATH = os.getenv("MODEL_PATH", "./model/cgan_complete.pth")  
     LATENT_DIM = int(os.getenv("LATENT_DIM", 100))
     IMAGE_SIZE = int(os.getenv("IMAGE_SIZE", 64))
+    LABEL_DIM = 4          # number of classes for conditioning
+    IMG_CHANNELS = 3       # RGB images
+    FEATURE_DIM = 64
     HOST = os.getenv("HOST", "0.0.0.0")
     PORT = int(os.getenv("PORT", 8002))
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
